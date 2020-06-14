a977b877fcc05cb32610126f2630c5c451504bcb
Kaiming Ouyang <kouya001@ucr.edu>
2020-06-14 16:18:17 -0500

    ch4/xpmem: use MPL gavl for xpmem segment cache
    Move the ch4/xpmem code to use the MPL GAVL code instead of its own
    implementation.

--- src/mpid/ch4/shm/ipc/xpmem/xpmem_mem.c
+++ src/mpid/ch4/shm/ipc/xpmem/xpmem_mem.c
@@ -14,7 +14,7 @@ int MPIDI_XPMEM_ipc_handle_map(int node_rank,
 
     mpi_errno = MPIDI_XPMEMI_seg_regist(node_rank, size, (void *) handle,
                                         vaddr,
-                                        &MPIDI_XPMEMI_global.segmaps[node_rank].segcache_ubuf);
+                                        MPIDI_XPMEMI_global.segmaps[node_rank].segcache_ubuf);
 
     MPIR_FUNC_VERBOSE_EXIT(MPID_STATE_MPIDI_XPMEM_IPC_HANDLE_MAP);
     return mpi_errno;
